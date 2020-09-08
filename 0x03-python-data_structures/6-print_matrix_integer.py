66869a3ec1a93b6f4b50eb48108ec7a1b61c8c35
ClaraCastaD <1980@holbertonschool.com>
2020-09-08 20:53:55 +0000

    fixing task number

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,9 @@ def print_matrix_integer(matrix=[[]]):
         for i in (matrix):
             k = 0
             for j in i:
-                if (k == len(i) -1):
+                k = k + 1
+                if (k == len(i)):
                     print("{:d}".format(j), end="")
                 else:
                     print("{:d}".format(j), end=" ")
-                k = k + 1
             print()
