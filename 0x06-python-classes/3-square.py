71847a45f1e47c305252819eda6359862330ee0c
ClaraCastaD <1980@holbertonschool.com>
2020-09-22 20:33:01 +0000

    Adding task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Class Square """
+
+
+class Square():
+    """ class Square """
+    def __init__(self, size = 0):
+        """ init class Square """
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """ Calculate area of the square """
+        return(self.__size * self.__size)
