6f9388e84fc556cfb53821708c2e93e658e59944
ClaraCastaD <1980@holbertonschool.com>
2020-09-22 22:51:11 +0000

    Fixing mistakes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,6 +4,6 @@
 
 class Square():
     """ class Square """
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """ init class Square """
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 class Square():
     """ class Square """
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """ init class Square """
         self.__size = size
         if type(size) is not int:
