7a9dfb73a5115cfa6084db08bcb5a8584f1ac51f
ClaraCastaD <1980@holbertonschool.com>
2020-09-22 20:19:53 +0000

    Adding task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Class Square """
+
+
+class Square():
+    """ class Square """
+    def __init__(self, size = 0):
+        """ init class Square """
+        self.__size = size
