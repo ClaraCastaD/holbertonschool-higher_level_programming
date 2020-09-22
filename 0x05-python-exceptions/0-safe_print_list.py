c3b728dd6414e5c063313259e64759ada2a78956
ClaraCastaD <1980@holbertonschool.com>
2020-09-22 02:00:27 +0000

    Fixing task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        for i in range(x):
-            print("{}".format(my_list[i]), end="")
+        for a in range(x):
+            print("{}".format(my_list[a]), end="")
             count += 1
-    except indexError:
+    except IndexError:
         pass
     print("")
     return count
