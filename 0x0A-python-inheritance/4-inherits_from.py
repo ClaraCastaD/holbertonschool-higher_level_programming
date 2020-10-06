29b69f4e0aa2ac1b66bc6a90410b20c0bf6f871d
ClaraCastaD <1980@holbertonschool.com>
2020-10-06 21:40:50 +0000

    Adding task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" function that returns True if the
+    object is an instance of a class """
+
+
+def inherits_from(obj, a_class):
+    """ Inherits from a class """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
