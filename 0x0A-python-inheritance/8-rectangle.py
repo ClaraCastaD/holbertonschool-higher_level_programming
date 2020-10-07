d62d374f8884704060f7c79a604682fd94d46d1a
ClaraCastaD <1980@holbertonschool.com>
2020-10-07 01:30:02 +0000

    Adding task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
-"""  Creating an empty class """
+"""define a rectangle"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry():
-    """ Init class """
-    pass
-
-    def area(self):
-        """ Public instance method """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Public instance method that
-        validates value """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+class Rectangle(BaseGeometry):
+    """create class"""
 
     def __init__(self, width, height):
+        """width vs height"""
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
