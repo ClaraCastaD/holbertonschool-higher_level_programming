4d27e4ad6a7b77d162e680ce96b21ceaf3ed9f8c
ClaraCastaD <1980@holbertonschool.com>
2020-10-08 03:28:20 +0000

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,5 @@
+
 #!/usr/bin/python3
-""" read a file """
-
-
 def read_file(filename=""):
-    with open(filename,  encoding='utf-8') as f:
-        print(f.read(), end="")
+    with open(filename, mode="r", encoding="utf-8") as MyFile:
+        print(MyFile.read(), end='')
