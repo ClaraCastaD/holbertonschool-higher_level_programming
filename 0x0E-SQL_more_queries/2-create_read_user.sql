7e739552183372365ff32285455a804acce991e5
ClaraCastaD <1980@holbertonschool.com>
2020-11-18 00:30:46 +0000

    Fixing task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,3 +2,4 @@
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 GRANT ALL PRIVILEGES ON hbtn_0d_2.* TO 'user_0d_2_pwd'@'localhost';
+FLUSH PRIVILEGES;
